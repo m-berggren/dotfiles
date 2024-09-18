8cd73496aa3bdcde9dbbc61d040abf9bcfb41084
Marcus Berggren <berggren.me@outlook.com>
2024-09-18 10:50:46 +0200

    Add fish config files

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,44 @@
+source ~/.config/fish/alias.fish
+
+if status --is-interactive
+	if type -q autojump
+		[ -f /home/linuxbrew/.linuxbrew/share/autojump/autojump.fish ]; and source /home/linuxbrew/.linuxbrew/share/autojump/autojump.fish
+	end
+	if type -q atuin
+		atuin init fish | source
+	end
+end
+
+# Load all saved ssh keys
+if test -x /usr/bin/ssh-add
+	/usr/bin/ssh-add -A &>/dev/null
+end
+
+# Fish syntax highlighting
+set -g fish_color_autosuggestion '555'  'brblack'
+set -g fish_color_cancel -r
+set -g fish_color_command --bold
+set -g fish_color_comment red
+set -g fish_color_cwd green
+set -g fish_color_cwd_root red
+set -g fish_color_end brmagenta
+set -g fish_color_error brred
+set -g fish_color_escape 'bryellow'  '--bold'
+set -g fish_color_history_current --bold
+set -g fish_color_host normal
+set -g fish_color_match --background=brblue
+set -g fish_color_normal normal
+set -g fish_color_operator bryellow
+set -g fish_color_param cyan
+set -g fish_color_quote yellow
+set -g fish_color_redirection brblue
+set -g fish_color_search_match 'bryellow'  '--background=brblack'
+set -g fish_color_selection 'white'  '--bold'  '--background=brblack'
+set -g fish_color_user brgreen
+set -g fish_color_valid_path --underline
+
+# Install Starship
+starship init fish | source
+
+# Zoxide
+zoxide init fish | source
