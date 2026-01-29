74c3e88a9018e59d1fc1af9c44c8a1e2e3e28ea5
Marcus Berggren <berggren.me@outlook.com>
2026-01-29 18:49:32 +0100

    Update files for fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -31,15 +31,17 @@ $MAMBA_EXE shell hook --shell fish --root-prefix $MAMBA_ROOT_PREFIX | source
 # Aliases
 alias cd="z" # zoxide
 alias ls="eza -la --color=always --group-directories-first"
-set -gx PATH /usr/sbin $PATH
 
 # Normally I use mise for handling zig verisons, 
 # but in this case I want specifically a new dev verison
 set -x PATH ~/.zig $PATH
 
+set -gx DOTNET_ROOT (mise where dotnet)
+set -gx PATH $DOTNET_ROOT $PATH
+
 # pnpm
 set -gx PNPM_HOME "/home/mbx/.local/share/pnpm"
 if not string match -q -- $PNPM_HOME $PATH
-  set -gx PATH "$PNPM_HOME" $PATH
+    set -gx PATH "$PNPM_HOME" $PATH
 end
 # pnpm end
